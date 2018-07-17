5751195f71cb300d304a69255367588f8a5e37af
Tiffany Jann <tiffanyjann@berkeley.edu>
2018-07-17 01:05:20 -0400

    got rid of library structure. everything is in one script now

--- pyTEDIE/tedie.py
+++ pyTEDIE/tedie.py
@@ -5,10 +5,55 @@ import numpy as np
 import glob
 from sklearn.preprocessing import MinMaxScaler
 from statsmodels.stats.descriptivestats import sign_test
-from mabc import MABC
 
 import logging
 logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s  [ %(levelname)-4s ] %(name)-6s  %(message)s')
+def calculate(data1, data2):
+    data1 = np.atleast_2d(data1)
+    data2 = np.atleast_2d(data2)
+    if data1.shape != data2.shape:
+        raise AssertionError('data1 and data2 dimensions do not match')
+    area = 0
+    for field in range(data1.shape[1]):
+        for datum in range(data1.shape[0]-1):
+            x1 = datum
+            x2 = datum+1
+            y1 = data1[datum, field]
+            y2 = data1[datum+1, field]
+            u1 = x1
+            u2 = x2
+            v1 = data2[datum, field]
+            v2 = data2[datum+1, field]
+            area += trapezoid(x1,y1,x2,y2,u1,v1,u2,v2)
+    return area
+
+def trapezoid(x1,y1,x2,y2,u1,v1,u2,v2):
+    """
+    Calculate area between two lines.
+    line1 = (x1, y1) -- (x2,y2)
+    line2 = (u1, v1) -- (u2, v2)
+    """
+    diff1 = v1 - y1
+    diff2 = v2 - y2
+    dif = diff1*diff2
+    if dif < 0: # then we know the lines intersect
+        m1 = (y2-y1)/(x2-x1)
+        b1 = y1 - m1*x1
+        m2 = (v2-v1)/(u2-u1)
+        b2 = v1 - m2*u1
+        xint = (-1)*(b2 - b1)/(m2-m1)
+        area1 = abs(v1-y1)*abs(xint-x1)/2 
+        area2 = abs(v2-y2)*abs(xint-x2)/2
+        area = area1 + area2
+    elif dif > 0: 
+        area = ( abs(diff1) + abs(diff2) ) * abs(x2-x1)/2
+    else:
+        ydif = max( abs(v2-y2), abs(v1-y1) )
+        xdif = abs(x2 - x1)
+        area = xdif*ydif/2
+    return area
 
 if __name__ == '__main__':
     ## USAGE:
@@ -50,12 +95,10 @@ if __name__ == '__main__':
     dict_data_discretized = {}
     if usenp:
         for f in glob.glob(discretizeddirectory + prefix + '*.npy'):
-            print('loading data from ' + f)
             logger.info('loading data from %s', f)
             dict_data_discretized[f] = np.load(f)
     else:
         for f in glob.glob(discretizeddirectory + prefix + '*.csv'):
-            print('loading data from ' + f)
             logger.info('loading data from %s', f)
             dict_data_discretized[f] = np.loadtxt(f, delimiter=',')
     
@@ -69,16 +112,13 @@ if __name__ == '__main__':
             raise AssertionError('please make sure your data does not have nan values')
         statistic, pvalue = sign_test(data_original.flatten(), data_d_s.flatten()) # this is different than the original code. flatten() turns the T x D matrix into one vector
         if pvalue < alpha:
-            print(discretization + ' failed the sign test, assigning inf mabc')
             logger.info('%s failed the sign test, assigning inf mabc', discretization)
             mabc_values[discretization] = float("inf")
         else:
-            mabc = MABC()
-            mabc_values[discretization] = mabc.calculate(data_d_s, data_original)
+            mabc_values[discretization] = calculate(data_d_s, data_original)
 
-    print(mabc_values)
+    logger.info('mabc values: %s', mabc_values)
     best = min(mabc_values, key=mabc_values.get)
-    print('best discretization:', best, mabc_values[best])
     logger.info('best discretization: %s with mabc = %s', best, mabc_values[best])
 
 
--- pyTEDIE/tests/test_mabc.py
+++ pyTEDIE/tests/test_mabc.py
@@ -1,24 +1,20 @@
 import unittest
 import numpy as np
-from mabc import MABC
-class TestMABC(unittest.TestCase):
+class Tests(unittest.TestCase):
 
     def setUp(self):
         """
         Runs first, every time.
         """
-        print('Testing MABC')
 
     def testcalculate(self):
         data1 = np.array([[1], [0]])
         data2 = np.array([[0.5], [0.5]])
-        mabc = MABC()
-        area = mabc.calculate(data1, data2)
+        area = calculate(data1, data2)
         self.assertEqual(0.25, area)
 
     def testtrapezoid(self):
-        mabc = MABC()
-        area = mabc.trapezoid(0,1,1,0,0,0.5,1,0.5)
+        area = trapezoid(0,1,1,0,0,0.5,1,0.5)
         self.assertEqual(0.25, area)
 
 if __name__ == '__main__':
